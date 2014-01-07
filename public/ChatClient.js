4291ec8a9720f05ca00ca1d755d0c81a1353731c
tponguyen86 <tponguyen86@gmail.com>
2014-01-07 23:05:37 +0700

    Commit Source

---
+++ public/ChatClient.js
@@ -0,0 +1,35 @@
+﻿/**
+* Created by TPOGroup
+* Date: 6/1/2014 DD/MM/YYYY
+* Time: 8:00 PM
+*/
+
+var Chat = {
+    socket  : null,
+    send    : function(){
+        var data = {
+            user    :document.getElementById("username").value,
+            message :document.getElementById("message").value
+        }
+       document.getElementById("message").value="";
+        Chat.socket.emit('send', data)
+    },
+
+    show    : function(data){
+        var content = data.user + ':' + data.message;
+        document.getElementById('Result').insertAdjacentHTML( 'beforeend', "<p>"+ content+"</p>");
+    },
+
+    greeting: function(data){
+        alert(data);
+    },
+
+    start   : function(url){
+        this.socket = io.connect(url);
+        this.socket.on('new_message', this.show);
+        this.socket.on('greeting', this.greeting);
+    }
+
+}
+    Chat.start('/');
+
---
+++ server.js
@@ -0,0 +1,51 @@
+﻿/**
+* Created by TPOGroup
+* Date: 6/1/2014 DD/MM/YYYY
+* Time: 8:00 PM
+*/
+
+var   http        = require('http')
+    , fs          = require('fs')
+    , socketIO    = require('socket.io')
+    , port        = process.env.PORT || 8085
+    , ip          = process.env.IP || '127.0.0.1';
+
+var serverConfig  = function(req, res){
+    if(req.url == '/'){
+        fs.readFile('./public/index.html',function(error,data){
+            if(error) {
+                throw error;
+            } else {
+                res.writeHead(200,{'Content-Type':'text/html'});
+                res.end(data);
+            }
+        })
+    }
+    if(req.url == '/ChatClient.js'){
+        fs.readFile('./public/ChatClient.js',function(error,data){
+            if(error){
+                throw error;
+            } else {
+                res.writeHead(200,{'Content-Type':'text/javscript'});
+                res.end(data);
+            }
+        })
+    }
+}
+var server      = http.createServer(serverConfig).listen(port, ip, function(){console.log('Server running at %s:%s', ip, port)})
+var io          = socketIO.listen(server);
+
+var run = function(socket){
+    console.log('New Client Connected');
+    socket.emit('greeting', 'Welcome to Socket.IO');
+    socket.on('send', function(data){
+        io.sockets.emit('new_message', data);
+        console.log('Forward message');
+    })
+}
+
+io.set('match origin protocol', true);
+io.set('origins', '*:*');
+//io.set('log level', 1);
+
+io.sockets.on('connection', run);
